3a44fca24cdfb5194a694183da28d3845bc697c2
John <johnnjau202@gmail.com>
2024-06-13 20:44:46 +0300

    5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Create trigger reset attribute valid_email
+DELIMITER //
+CREATE TRIGGER update_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+//
+
+DELIMITER ;
